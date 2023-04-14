e8964bca5e9503090677bfdf149cf98ee8ee3d40
Luis Rachope <luis.chiasso@hotmail.com>
2023-04-14 17:03:05 -0300

    Criação string de conexão com o SqlSever

---
+++ AlunosApiReact/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AlunosApiReact.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlunosApiReact.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
