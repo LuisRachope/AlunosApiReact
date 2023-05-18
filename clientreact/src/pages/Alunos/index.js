c1e1834d10fdf357fd8a4d53c5939dc595cf29f2
Luis Rachope <luis.chiasso@hotmail.com>
2023-05-18 20:45:20 -0300

    Criação no react da autenticação e do logout utilizando api e jwt

--- clientreact/src/pages/Alunos/index.js
+++ clientreact/src/pages/Alunos/index.js
@@ -1,20 +1,53 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
 import './styles.css';
+import api from '../../services/api';
 
 import logoCadastro from '../../assets/cadastro1.png';
+import {FiXCircle, FiEdit, FiUserX} from 'react-icons/fi';
 
-import {FiXCircle, FiEdit, FiUserX} from 'react-icons/fi'
+
 
 export default function Alunos() {
+
+    const[nome, setNome] = useState('');
+    const[alunos,setAlunos] = useState([]);
+
+    const email = localStorage.getItem('email');
+    const token = localStorage.getItem('token');
+
+    const history = useNavigate();
+
+    const authorization = {
+        headers : {
+            Authorization : `Bearer ${token}`
+        }
+    }
+
+    useEffect( ()=> {
+        api.get('/api/alunos', authorization).then(
+            response=> {setAlunos(response.data);
+            }, token)
+    })
+
+    async function logout(){
+        try{
+            localStorage.clear();
+            localStorage.setItem('token', '');
+            authorization.headers = '';
+            history('/');
+        }catch(err){
+            alert('Não foi possível fazer o logout' + err)
+        }
+    }
 
     return (
         <div className="aluno-container">
             <header>
                <img src={logoCadastro} alt="Cadastro" />
-               <span>Bem-Vindo, <strong></strong>!</span>
+               <span>Bem-Vindo, <strong>{email}</strong>!</span>
                <Link className="button" to="/aluno/novo/0">Novo Aluno</Link>
-               <button type="button" class="center-button">
+               <button onClick={logout} type="button" class="center-button">
                     <div class="icon-wrapper">
                         <FiXCircle size={45} color='#17202a'></FiXCircle>
                     </div>
@@ -30,34 +63,21 @@ export default function Alunos() {
 
             <h1>Relação de Alunos</h1>
             <ul> 
-                <li>
-                    <b>Nome:</b>Paulo<br/><br/>
-                    <b>Email:</b>paulo@email.com<br/><br/>
-                    <b>Idade:</b>22<br/><br/>
-                    <button type="button">
-                        <FiEdit size='25' color='#17202a'></FiEdit>
-                    </button>
+                {alunos.map(aluno=>(
+                    <li key={aluno.id}>
+                        <b>Nome:</b>{aluno.nome}<br/><br/>
+                        <b>Email:</b>{aluno.email}<br/><br/>
+                        <b>Idade:</b>{aluno.idade}<br/><br/>
 
-                    <button type="button"> 
-                        <FiUserX size='25' color='#17202a'></FiUserX>
-                    </button>
-                </li>
-            </ul>
-            <br/>
-            <ul>
-                <li key='{aluno.id}'>
-                <b>Nome:</b><br/><br/>
-                <b>Email:</b><br/><br/>
-                <b>Idade:</b><br/><br/>
+                        <button type="button"> 
+                            <FiEdit size='25' color='#17202a'></FiEdit>
+                        </button>
 
-                <button type="button"> 
-                    <FiEdit size='25' color='#17202a'></FiEdit>
-                </button>
-
-                <button type="button"> 
-                    <FiUserX size='25' color='#17202a'></FiUserX>
-                </button>
-               </li>
+                        <button type="button"> 
+                            <FiUserX size='25' color='#17202a'></FiUserX>
+                        </button>
+                    </li>
+                ))}
             </ul>
         </div>
      );
